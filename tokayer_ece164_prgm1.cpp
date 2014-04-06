4e81e43fa0082536fe0a9711e62cca280a79228c
Yaron Tokayer <yaronmtokayer@gmail.com>
2014-04-06 13:43:59 -0400

    first buggy version :)

--- tokayer_ece164_prgm1.cpp
+++ tokayer_ece164_prgm1.cpp
@@ -54,17 +54,21 @@ class SimpleList{
 		void push_front( const T & x ){ // method to push at front of list
 			Node *newnode =	new Node( x, front );
 			front = newnode;
+			if ( isempty() ) // list is empty
+				back = newnode;
 		}
 		void push_back( const T & x ){ // method to push at back of list
 			Node *newnode = new Node( x );
 			back = newnode;
+			if( isempty() ) // list is empty
+				front = newnode;
 		}
 		T pop_front( ){ //method to pop from the front of a list and return value
-			T tmp1 = front.data;
-			Node *tmp2 = front.next;
+			T d = front->data;
+			Node *p = front.next;
 			delete front;
-			front = ptr;
-			return data;
+			front = p;
+			return d;
 		}
 		
 		virtual void push( const T & x ) // pure virtual functions
@@ -73,6 +77,9 @@ class SimpleList{
 	public:
 		string getname() const{ // function to return name of stack or queue
 			return name;
+		}
+		bool isempty() const{ // function to check for emptiness
+			return front == NULL;
 		}
 }
 
@@ -108,68 +115,125 @@ class Queue: public SimpleList{
 // function to find list with given name
 template <typename T>
 SimpleList<T>* findlist( const string & lname, list<SimpleList<T> *> & Tlist){
-	for (list<SimpleList<T> *>::iterator SLp=Tlist.begin(); SLp != Tlist.end(); ++SLp){ // check for valid name
-		if ( SLp->getname() == lname );
-			return SLp;
+	for ( list<SimpleList<T> *>::iterator SLp=Tlist.begin(); SLp != Tlist.end(); ++SLp ){ // check for valid name
+		if ( (*SLp)->getname() == lname );
+			return *SLp;
 	}
 	return NULL; // if name doesn't exist, return pointer to NULL
 }
 
 // function to proccess each line of input
-void procline( const string & ll, list & ilist, list & dlist, list & slist, fstream & output ){
+void procline( const string & ll, list<SimpleList<int> *> & ilist, list<SimpleList<double> *> & dlist, list<SimpleList<string> *> & slist, fstream & output ){
 	istringstream iss( ll );
-	string cmd;
+	string cmd; // command
 	iss >> cmd;
+	string lname; // name of list to be modified
+	iss >> lname;
 	if ( cmd == "create" ) { // for create command
-		string lname; // name of list to be created
-		iss >> lname;
 		if ( lname[1] == "i" ){	// list to be created is of int type
-			if ( findlist( lname, ilist ) == NULL )// name doesn't already exist
+			if ( findlist( lname, ilist ) == NULL ){ // name doesn't already exist
 				string spec; // is list of integers a stack or a queue
 				iss >> spec;
-				if ( spec == "queue" )
+				if ( spec == "queue" ){ // create new queue of ints with desired name
 					SimpleList<int>* newq = new Queue( lname );
 					ilist.push_front( newq );
-				else if ( spec == "stack" )
+				}
+				else if ( spec == "stack" ){ // create new stack of ints with desired name
 					SimpleList<int>* newst = new Stack( lname );
 					ilist.push_front( newst );
+				}
+			}
 			else // name already exists
 				output << "ERROR: This name already exists!";
 		}
 
 		else if ( lname[1] == "d" ){ // list to be created is of double type
-			if ( findlist( lname, dlist ) == NULL )// name doesn't already exist
+			if ( findlist( lname, dlist ) == NULL ){ // name doesn't already exist
 				string spec; // is list of strings a stack or a queue
 				iss >> spec;
-				if ( spec == "queue" )
+				if ( spec == "queue" ){ // create new queue of doubles with desired name
 					SimpleList<double>* newq = new Queue( lname );
 					ilist.push_front( newq );
-				else if ( spec == "stack" )
+				}
+				else if ( spec == "stack" ){ // create new stack of doubles with desired name
 					SimpleList<double>* newst = new Stack( lname );
 					ilist.push_front( newst );
+				}
+			}
 			else // name already exists
 				output << "ERROR: This name already exists!";
 		}
 
 		else if ( lname[1] == "s" ){ // list to be created is of string type
-			if ( findlist( lname, slist ) == NULL )// name doesn't already exist
-				string spec; // is list of strings a stack or a queue
+			if ( findlist( lname, slist ) == NULL ){ // name doesn't already exist
+				string spec; // is list of strings a stack or a queue?
 				iss >> spec;
-				if ( spec == "queue" )
+				if ( spec == "queue" ){ // create new queue of strings with desired name
 					SimpleList<string>* newq = new Queue( lname );
 					ilist.push_front( newq );
-				else if ( spec == "stack" )
+				}
+				else if ( spec == "stack" ){ // create new stack of ints with desired name
 					SimpleList<string>* newst = new Stack( lname );
 					ilist.push_front( newst );
+				}
+			}
 			else // name already exists
-				output << "ERROR: This name already exists!";
+				output << "ERROR: This name already exists!" << '\n';
 		}
 	}
 	
-else if ( cmd == "push" ) { // for push command
-}
+	else if ( cmd == "push" ) { // for push command
+		if ( lname[1] == "i" ){	// data to be pushed is of int type
+			lptr = findlist( lname, ilist );
+			if ( lptr == NULL ) // name doesn't exist
+				output << "ERROR: This name does not exist!" << '\n';
+			else{ // name exists
+				int pushdata;
+				iss >> pushdata;
+				lptr->push( pushdata );
+			}
+		}
+		else if ( lname[1] == "d" ){	// data to be pushed is of double type
+			lptr = findlist( lname, dlist );
+			if ( lptr == NULL ) // name doesn't exist
+				output << "ERROR: This name does not exist!" << '\n';
+			else{ // name exists
+				double pushdata;
+				iss >> pushdata;
+				lptr->push( pushdata );
+			}
+		}
+		else if ( lname[1] == "s" ){	// data to be pushed is of string type
+			lptr = findlist( lname, slist );
+			if ( lptr == NULL ) // name doesn't exist
+				output << "ERROR: This name does not exist!" << '\n';
+			else{ // name exists
+				string pushdata;
+				iss >> pushdata;
+				lptr->push( pushdata );
+			}
+		}		
+	}
 
-else if ( cmd == "pop" ) { // for pop command
+	else if ( cmd == "pop" ) { // for pop command
+		if ( lname[1] == "i" ) // list to be popped contains ints
+			lptr = findlist( lname, dlist );
+		else if ( lname[1] == "d" ) // list to be popped contains doubles
+			lptr = findlist( lname, ilist );
+		else if ( lname[1] == "s" ) // list to be popped contains strings
+			lptr = findlist( lname, slist );
+		
+		if ( lptr == NULL ) // name doesn't exist
+			output << "ERROR: This name does not exist!" << '\n';
+		else{ // name exists
+			if ( lptr->isempty() )// list is empty
+				output << "ERROR: This list is empty!" << '\n';
+			else
+				val = lptr->pop();
+				output << "Value popped: " << val << '\n';
+		}
+
+	}
 }
 
 int main()
@@ -187,11 +251,10 @@ int main()
 	
 	int i = 1;
 	string currline; // string with current line
-	output << "PROCESSING COMMAND: " << currline;
+	output << "PROCESSING COMMAND: " << currline << '\n';
 	while( getline (input, currline) ) // test for existence of line
 	{
-		/* output << currline << '\n';
-		++i; */
+		procline( currline, listSLi, listSLd, listSLs, output );
 	}
 	
 	// close input and output streams
